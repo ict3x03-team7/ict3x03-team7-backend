848e44c6c69528d1d67ced50c9b1fc58a86e3f66
Ismael <muhammadismaelosman@gmail.com>
2023-09-11 22:25:08 +0800

    setup getDummyByName jest test

--- src/modules/dummy/usecases/getDummyByName/getDummyByName.test.js
+++ src/modules/dummy/usecases/getDummyByName/getDummyByName.test.js
@@ -1,7 +1,15 @@
-test("returns 5 when 2 is added to 3", () => {
-  let validity;
-  if (2 + 3) {
-    validity = true;
-  }
-  expect(validity).toBe(true);
+const request = require("supertest");
+const app = require("./../../../../app");
+
+describe("GET /dummy/:dummyName", () => {
+  test("given an valid dummy name, should respond with a 200 status code", async () => {
+    const response = await request(app).get("/dummy/dummy1");
+    // console.log(response._body.result);
+    expect(response.statusCode).toBe(200);
+    expect(response._body.result.dummyName).toBe("dummy1");
+  });
+  test("given an invalid dummy name, should respond with a 400 status code", async () => {
+    const response = await request(app).get("/dummy/dummy5");
+    expect(response.statusCode).toBe(400);
+  });
 });
