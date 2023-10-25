5557021c4dcf21d002eb895d0fed7f559e1503f5
Ismael <muhammadismaelosman@gmail.com>
2023-10-25 13:27:48 +0800

    edited validateEmail

--- src/shared/utils/validateEmail.js
+++ src/shared/utils/validateEmail.js
@@ -1,5 +1,5 @@
 function isValidEmail(email) {
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
   return emailRegex.test(email);
 }
 
